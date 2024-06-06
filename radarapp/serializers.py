7c275349adb0b146346b8ba81d7368a5ac484e23
HabeebFF <habeebmuftau05@gmail.com>
2024-06-06 17:17:40 +0100

    Newly

--- radarapp/serializers.py
+++ radarapp/serializers.py
@@ -4,12 +4,12 @@ from .models import Users
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = Users
-        fields = ('email', 'username', 'full_name', 'password', 'is_superuser', 'is_active', 'user_type')
+        fields = ('full_name', 'email', 'username', 'password', 'is_superuser', 'is_active', 'user_type')
         extra_kwargs = {'password': {'write_only': True}}
 
 
 class DriverSerializer(serializers.ModelSerializer):
     class Meta:
         model = Users
-        fields = ('email', 'username', 'full_name', 'password', 'is_superuser', 'is_active', 'user_type')
+        fields = ('full_name', 'email', 'username', 'password', 'is_superuser', 'is_active', 'user_type')
         extra_kwargs = {'password': {'write_only': True}}
